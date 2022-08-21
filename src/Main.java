369b36a65e0b5863f0e3389c9f12d1ea358d07df
Алекс <dendyboy777@gmail.com>
2022-08-21 11:19:32 +0300

    hw 5-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args){
-     
-    }
-}
+     int[] weigths =new int[]{2,3,4,5,6,7,9,10,0,0};
+        weigths[0] = 90;
+        int januvaryWeight= weigths[0];
+        System.out.println(weigths[2]);
+        for (int i = 0; i< weigths.length; i++){
+            System.out.println(weigths[i]);}
+// задание 1
+
+int[] q = {1,2,3};
+float[] w = {1.57f,7.654f,9.986f};
+int [] e = {10,20,30};
+// задание 2
