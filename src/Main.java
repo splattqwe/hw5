7d7dfc1b1465bacbd034653410a6e79105317c51
Алекс <dendyboy777@gmail.com>
2022-08-21 12:16:13 +0300

    hw 5-1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
+
+
 public class Main {
     public static void main(String[] args){
-     int[] weigths =new int[]{2,3,4,5,6,7,9,10,0,0};
+        int[] weigths =new int[]{2,3,4,5,6,7,9,10,0,0};
         weigths[0] = 90;
         int januvaryWeight= weigths[0];
-        System.out.println(weigths[2]);
         for (int i = 0; i< weigths.length; i++){
             System.out.println(weigths[i]);}
+        System.out.println();
 // задание 1
-
-int[] q = {1,2,3};
-float[] w = {1.57f,7.654f,9.986f};
-int [] e = {10,20,30};
+        int[] q =new int[]{1,2,3};
+        float[] w = {1.57f,7.654f,9.986f};
+        int [] e = {10,20,30};
 // задание 2
+        for (int i = 0; i < q.length; i++) {
+            System.out.print(q[i]+ ",");{
+            for (float a = 0; a < w.length; a++) {
+                System.out.print(w[a]);}}}}}
