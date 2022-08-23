3156fb672074a6da52ac1e29acbd3a6036c14a7c
Алекс <dendyboy777@gmail.com>
2022-08-23 19:28:46 +0300

    hw 5-3

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,40 @@
 
 
 public class Main {
-    public static void main(String[] args){
-        int[] weigths =new int[]{2,3,4,5,6,7,9,10,0,0};
-        weigths[0] = 90;
-        int januvaryWeight= weigths[0];
-        for (int i = 0; i< weigths.length; i++){
-            System.out.println(weigths[i]);}
-        System.out.println();
-// задание 1
-        int[] q =new int[]{1,2,3};
-        float[] w = {1.57f,7.654f,9.986f};
-        int [] e = {10,20,30};
-// задание 2
-        for (int i = 0; i < q.length; i++) {
-            System.out.print(q[i]+ ",");}
-        System.out.println();
-        {
-            for (int a = 0; a < w.length; a++) {
-                System.out.print(w[a] + ",");}
-            System.out.println();
-            for (int s = 0; s <e.length ; s++) {
-                System.out.print(e[s]+"," );
+    public static void main(String[] args) {
+        //        int[] weigths =new int[]{2,3,4,5,6,7,9,10,0,0};
+        //        weigths[0] = 90;
+        //       int januvaryWeight= weigths[0];
+        //       for (int i = 0; i< weigths.length; i++){
+        //           System.out.println(weigths[i]);}
+        //       System.out.println();
+        // задание 1
+        //      int[] q =new int[]{1,2,3};
+        //     float[] w = {1.57f,7.654f,9.986f};
+        //      int [] e = {10,20,30};
+        // задание 2
+        //       for (int i = 0; i < q.length; i++) {//          System.out.print(q[i]+ ",");}
+        //       System.out.println();
+        //
+        //       for (int a = 0; a < w.length; a++) {
+        //               System.out.print(w[a] + ",");}
+        //           System.out.println();
+        //       for (int s =0; s< e.length ;s++){
+        //               System.out.print(e[s]+"," );
 
-            }}}}
 
+        // задание 3
+
+        int[] arr = new int[]{1, 2, 3};
+ //      for (int i = 0; i < arr.length; i++) {
+  //          System.out.println(arr[i]);
+
+        for (int i = arr.length-1;i>-1; i--) {
+            System.out.print(arr[i]+ ",");
+
+        }
+
+
+        }
+
+    }
